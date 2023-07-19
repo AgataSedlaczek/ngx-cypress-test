50bfaa0c3274175416c275b0dd8e9fa4194994f8
AgataSedlaczek <navi2@o2.pl>
2023-07-19 19:50:41 +0200

    Update firstTest.spec.js

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,43 @@
+
+
+describe('our first suite', () => {
+
+    describe('our suite section', () => {
+
+        beforeEach('code for every test', () => {
+            //repetive code
+        })
+        it('some test name', ( => {
+
+        }))
+    })
+
+    it('first test', () => {
+
+    })
+
+    it('second test', () => {
+
+    })
+
+    it('third test', () => {
+
+    })
+
+})
+
+describe('our second suite', () => {
+
+    it('first test', () => {
+
+    })
+
+    it('second test', () => {
+
+    })
+
+    it('third test', () => {
+
+    })
+
+})
